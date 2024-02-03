1d510dfcf194d48b91517c95e975e1162aee7f45
Saharsh Wadekar <saharshwadekar.21@stvincentngp.edu.in>
2024-02-04 00:37:01 +0530

    comments added

--- Data Analysis And Algorithm/1_Practical/newfilemethod.cpp
+++ Data Analysis And Algorithm/1_Practical/newfilemethod.cpp
@@ -7,35 +7,38 @@
 using namespace std;
 using namespace std::chrono;
 
+// function to generate randome number and save it in file
 void generateAndSaveRandomNumbers(const string& fileName, int size) {
-    ofstream file(fileName);
-    if (!file.is_open()) {
+    ofstream file(fileName); // file open for writing
+    if (!file.is_open()) { // Open file error detection
         cerr << "Error opening file for writing: " << fileName << endl;
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); // exit with failure status
     }
 
-    srand(static_cast<unsigned>(time(0)));
-    for (int i = 0; i < size; ++i) {
-        file << rand() % 1000 << " ";
+    srand(static_cast<unsigned>(time(0))); // seed assigned
+    for (int i = 0; i < size; ++i) { // store to the file
+        file << rand() % 1000 << " "; // random(0 to 999) generated
     }
 
-    file.close();
+    file.close(); // closing the file
 }
 
+// Function to read numbers from a file and save them in an array
 void readNumbersFromFile(const string& fileName, int arr[], int size) {
-    ifstream file(fileName);
-    if (!file.is_open()) {
+    ifstream file(fileName); // open file for reading
+    if (!file.is_open()) {  // Open file error detection
         cerr << "Error opening file for reading: " << fileName << endl;
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); // exit with failure status
     }
 
     for (int i = 0; i < size; ++i) {
-        file >> arr[i];
+        file >> arr[i];  // inserting data to the array form file
     }
 
-    file.close();
+    file.close(); // closing file
 }
 
+// funciton of an bubble sort implementation
 void bubbleSort(int arr[], int size) {
     for (int i = 0; i < size - 1; ++i) {
         for (int j = 0; j < size - i - 1; ++j) {
@@ -45,16 +48,16 @@ void bubbleSort(int arr[], int size) {
         }
     }
 }
-
+// funciton of an seleciton sort implementation
 void selectionSort(int arr[], int size) {
     for (int i = 0; i < size - 1; ++i) {
-        int minIndex = i;
+        int minIndex = i;  // getting least value array index
         for (int j = i + 1; j < size; ++j) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
-        swap(arr[i], arr[minIndex]);
+        swap(arr[i], arr[minIndex]); // lowest put forward
     }
 }
 
@@ -65,12 +68,14 @@ int main() {
     const int maxArraySize = 1000; // Set the maximum array size for testing
     const int stepSize = 50;      // Set the step size for increasing array size
 
+    //opening respective files
     ofstream bubbleSortFile(bubbleSortFileName);
     ofstream selectionSortFile(selectionSortFileName);
 
     bubbleSortFile << "Array Size,Time (Bubble Sort)" << endl;
     selectionSortFile << "Array Size,Time (Selection Sort)" << endl;
 
+    // Impliment stepwise manner array of number increase
     for (int arraySize = stepSize; arraySize <= maxArraySize; arraySize += stepSize) {
         // Generate and save random numbers
         generateAndSaveRandomNumbers(inputFileName, arraySize);
@@ -86,7 +91,7 @@ int main() {
         auto durationBubble = duration_cast<microseconds>(stopBubble - startBubble);
         bubbleSortFile << arraySize << "," << durationBubble.count() << ",";
         for (int i = 0; i < arraySize; ++i)
-        {
+        {   // storing into file
             bubbleSortFile << numbers[i] << " ";
         }
         bubbleSortFile << endl;
@@ -101,15 +106,17 @@ int main() {
         auto durationSelection = duration_cast<microseconds>(stopSelection - startSelection);
         selectionSortFile << arraySize << "," << durationSelection.count() << ",";
         for (int i = 0; i < arraySize; ++i)
-        {
+        {   
             selectionSortFile << numbers[i] << " ";
         }
         selectionSortFile << endl;
     }
 
+    // closing the files where opened
     bubbleSortFile.close();
     selectionSortFile.close();
 
+    // successfull message to terminal
     cout << "Sorting and data collection completed. Results saved in CSV files." << endl;
 
     return 0;
