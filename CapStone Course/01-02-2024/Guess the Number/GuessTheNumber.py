a58be1c1c8a96df42d62d33f4467ca035cb0f36f
Saharsh Wadekar <saharshwadekar.21@stvincentngp.edu.in>
2024-02-08 00:44:24 +0530

    hello

--- CapStone Course/01-02-2024/Guess the Number/GuessTheNumber.py
+++ CapStone Course/01-02-2024/Guess the Number/GuessTheNumber.py
@@ -1,9 +1,27 @@
 import random
+import sys
 
 computer = random.randint(1,10)
+computer = 8;
 print("range 1 to 10")
-num = 6 
 
-print("Computer : " , computer)
-print("input : " , num)
-print(num==computer)
+guess = 0
+chance = 3;
+while(chance):
+    try:
+        chance -= 1;
+        guess = int(input("Enter Your Guess :"));
+        if(guess < computer):
+            print(f"{chance}'s left: Guess Higher Number")
+        elif(guess > computer):
+            print(f"{chance}'s left: Guess Lower Number")
+        elif(guess==computer):
+            print("Congratulation! You Guessed it Correct.")
+
+    except Exception as e:
+        print(e);
+        sys.exit(1);
+
+
+if(guess!=computer and chance==0):
+    print("Sorry You loose the Game\nCorrect Answer :",computer);
