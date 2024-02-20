5d42aa6c82c6b6b080ab7966232284c29290e53d
Saharsh Wadekar <saharshwadekar.21@stvincentngp.edu.in>
2024-02-20 20:23:03 +0530

    remove

--- Advanced Computer Networking/P2-07-02-2024.py
+++ Advanced Computer Networking/P2-07-02-2024.py
@@ -1,27 +1,32 @@
+'''
+    @student     : Saharsh Sanjay Wadekar
+    @Sec/Rollno. : B - 48
+'''
 import sys
 
-def intergerToBinary(arrAddress):
-    binAddress = []
-    for octet in arrAddress:
-        n = bin(octet)[2:]
-        n = n.zfill(8)
-        binAddress.append(n);
-    return binAddress
-
-def identifyClass(binAddress):
-    firstOctet = binAddress[0]
-    if '0' == firstOctet[0]:
-        return "A"
-    elif '10' == firstOctet[0:2]:
-        return "B"
-    elif '110' == firstOctet[0:3]:
-        return "C"
-    elif '1110' == firstOctet[0:4]:
-        return "D"
-    else:
-        return "E"
+def calculateMaskValue(cusMask):
+    i = 0
+    octet = 0;
+    if (cusMask > 32):
+        return "Invalid Custome Mask Given!"
+    if (cusMask >= 24):
+        i = (cusMask - 24)
+        for x in range(i):
+            octet += pow(2,7-x)
+        return f"255.255.255.{octet}"
+    elif (cusMask >= 16):
+        i = (cusMask - 16)
+        for x in range(i):
+            octet += pow(2,7-x)
+        return f"255.255.{octet}.0"
+    elif (cusMask >= 8):
+        i = (cusMask - 8)
+        for x in range(i):
+            octet += pow(2,7-x)
+        return f"255.{octet}.0.0"
 
 def identifyNetAddNetHostId(ipAddress,ipClass):
+    ipAddress = list(map(str,ipAddress))
     match ipClass:
         case 'A':
             return ipAddress[0] + ".0.0.0" , '.'.join(ipAddress[:1]), '.'.join(ipAddress[1:])
@@ -36,92 +41,88 @@ def identifyNetAddNetHostId(ipAddress,ipClass):
     return "", "", ""
 
 def findFLTIpAddress(ipAddress,ipClass):
+    ipAddress = list(map(str,ipAddress))
     match ipClass:
         case 'A':
-            return ipAddress[0] + ".0.0.0" , '.'.join(ipAddress[0]) + ".255.255.255" , 255*255*255
+            return ipAddress[0] + ".0.0.0" , ipAddress[0] + ".255.255.255" , 255*255*255
         case 'B':
             return '.'.join(ipAddress[:2]) + ".0.0", '.'.join(ipAddress[:2]) + ".255.255", 255*255
         case 'C':
             return '.'.join(ipAddress[:3]) + ".0",  '.'.join(ipAddress[:3]) + ".255", 255
     return "", "", 0
 
-def calculateMaskValue(cusMask):
-        i = 0
-        octet = 0;
-        if (cusMask >= 24):
-            i = (cusMask - 24)
-            for x in range(i):
-                octet += pow(2,7-x)
-            return f"255.255.255.{octet}"
-        elif (cusMask >= 16):
-            i = (cusMask - 16)
-            for x in range(i):
-                octet += pow(2,7-x)
-            return f"255.255.{octet}.0"
-        elif (cusMask >= 8):
-            i = (cusMask - 8)
-            for x in range(i):
-                octet += pow(2,7-x)
-            return f"255.{octet}.0.0"
+def identifyClass(binAddress):
+    firstOctet = binAddress[0]
+    if '0' == firstOctet[0]:
+        return "A"
+    elif '10' == firstOctet[0:2]:
+        return "B"
+    elif '110' == firstOctet[0:3]:
+        return "C"
+    elif '1110' == firstOctet[0:4]:
+        return "D"
+    else:
+        return "E"
+
+def binToint(x):
+    return eval('0b'+f'{x}')
+
+def intergerToBinary(arrAddress):
+    binAddress = []
+    for octet in arrAddress:
+        n = bin(octet)[2:]
+        n = n.zfill(8)
+        binAddress.append(n);
+    return binAddress
 
 if __name__ == '__main__':
     # Ipv4 inputing string
+    choice = int(input("\nInputed IP Address will be in\n\t1.Interger\n\t2.Binary\n\nEnter Choice : "))
     ipAddress = input("Enter IPv4 Address :")
-    arrAddres = []
+
     try:
-        arrAddress = list(map(int,ipAddress.split(".")))
+        ipclist = list(map(str,ipAddress.split('/')))
+        ipAddress = ipclist[0];
+        cusMask = int(ipclist[1]);
+        ipAddress = list(map(int,ipAddress.split(".")))
+        if choice == 2:
+            ipAddress = list(map(binToint,ipAddress))
         print("Inputed Address in an valid integer string")
     except Exception as e:
         # Exception occur when one of the ipAddress is not in integer
         print(e)
         print("Inputed Address in not an integer string")
         sys.exit(1)     #system should exit the program i.e.,(exit == 1(error))
-    print(
+
+    binAddress = intergerToBinary(ipAddress)
+    printBinAddress = ".".join(binAddress)
+    printintAddress = list(map(str,ipAddress))
+    printintAddress = ".".join(printintAddress)
+
+    ipClass = identifyClass(binAddress)
+
+    netAdd, netId, hostId = identifyNetAddNetHostId(ipAddress,ipClass)
+
+    fIpAdd , lIpAdd , tIpAdd = findFLTIpAddress(ipAddress,ipClass)
+
+    customeMask = calculateMaskValue(cusMask)
+    bincus = list(map(int,str(customeMask).split(".")))
+    bincus = ".".join(intergerToBinary(bincus))
+    print(f'''
+        Ip (Integer) :{printintAddress}
+        Ip (Binary)  :{printBinAddress}
+
+        Class of Ip Address :{ipClass}
+
+        Your Network Address : {netAdd}
+        Your Network Id : {netId}
+        Your Host Id : {hostId}
+
+        First Ip Address : {fIpAdd}
+        Last Ip Address : {lIpAdd}
+        Total Ip Address : {tIpAdd}
+
+        Custom Masking Value : {customeMask}
+                               {bincus}
         '''
-        1. Convert To Binary Notation
-        2. Identify Class of Ip
-        3. Identify Network Address, Network Id, Host Id
-        4. Find First Address , Last Address , Total Address
-        5. Calculate Custome Subnet Mask
-        6. Exit
-        '''
-    )
-    binAddress = []
-    ipClass = "";
-    ipAddress = list(map(str,ipAddress.split(".")))
-    while(True):
-        choice = int(input("Enter Your Choice : "))
-        match choice:
-            case 1:
-                binAddress = intergerToBinary(arrAddress)
-                printBinAddress = ".".join(binAddress)
-                print(printBinAddress)
-            case 2:
-                ipClass = identifyClass(binAddress)
-                print("Class of Given Ip Address is :" , ipClass)
-            case 3:
-                netAdd, netId, hostId = identifyNetAddNetHostId(ipAddress,ipClass)
-                print(
-                    f'''
-                    Your Network Address : {netAdd}
-                    Your Network Id : {netId}
-                    Your Host Id : {hostId}
-                    ''')
-            case 4:
-                fIpAdd , lIpAdd , tIpAdd = findFLTIpAddress(ipAddress,ipClass)
-                print(
-                    f'''
-                    First Ip Address : {fIpAdd}
-                    Last Ip Address : {lIpAdd}
-                    Total Ip Address : {tIpAdd}
-                    ''')
-            case 5:
-                cusMask = int(input(f"Enter custome Subnet Mask : {'.'.join(ipAddress)}/"))
-                customeMask = calculateMaskValue(cusMask)
-                print(
-                    f'''
-                    Custom Masking Value : {customeMask}
-                    '''
-                      )
-            case 6:
-                sys.exit(0)
+          )
